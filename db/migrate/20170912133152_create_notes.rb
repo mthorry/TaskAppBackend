f0ac01b4eabbd7ca60f30c61b7d2fcc80cc05993
Neil Steiner <npsteiner@gmail.com>
2017-09-12 11:36:04 -0400

    added fields to form and added columns to notes table

--- db/migrate/20170912133152_create_notes.rb
+++ db/migrate/20170912133152_create_notes.rb
@@ -1,7 +1,16 @@
 class CreateNotes < ActiveRecord::Migration[5.1]
   def change
     create_table :notes do |t|
+      t.string :title
       t.string :body
+      t.integer :est_hours
+      t.integer :actual_hours
+      t.datetime :due_date
+      t.datetime :time_completed
+      t.string :location
+      t.integer :user_id
+      # tags will go on join table
+
 
       t.timestamps
     end
