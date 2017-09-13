ec08610cafce90fd66f051bb1d01744c2d236433
Neil Steiner <npsteiner@gmail.com>
2017-09-13 14:12:41 -0400

    filled out lists controller identicalyl to notes controller and everything is better and not broken

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::ListsController < ApplicationController
 
   def index
-    
+    @lists = List.all
+    render json: @lists, status: 200
   end
 
   def create
-    
+    @list = List.create(list_params)
+    render json: @list, status: 201
   end
 
   private
