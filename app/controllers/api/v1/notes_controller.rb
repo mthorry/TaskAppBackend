2055fbe7155cc13f2959a54df7c7c8dbed97147e
Neil Steiner <npsteiner@gmail.com>
2017-09-14 16:01:04 -0400

    we did something to the controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -6,17 +6,17 @@ class Api::V1::NotesController < ApplicationController
   end
 
   def create
-
     @note = Note.create(note_params)
     render json: @note, status: 201
   end
 
   def update
-    
+    @note = Note.find(params[:id])
+    @note.update(note_params)
+    render json: @note, status:200
   end
 
   def destroy
-    
     @note = Note.find(params[:id])
     @note.destroy
     render json: @note, status: 200
@@ -24,7 +24,7 @@ class Api::V1::NotesController < ApplicationController
 
   private
     def note_params
-      params.permit(:title, :body, :est_hours, :due_date, :location, :list_id)
+      params.permit(:title, :body, :est_hours, :due_date, :location, :list_id, :actual_hours, :time_completed)
     end
 
 end
